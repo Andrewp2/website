553a50f352edc1152195c98544f653486fa611b9
andrewp2 <andrewp2@iastate.edu>
2019-12-23 18:07:21 -0600

    Add player

--- js/Maze.js
+++ js/Maze.js
@@ -1,8 +1,10 @@
-const size = 41;
+const size = 21;
 const numRows = size;
 const numCols = size;
 var maze;
 var walls;
+var player;
+var playerPosition = {x: 0, z: 1};
 let scene;
 
 function main() {
@@ -22,7 +24,7 @@ function main() {
     0.1,
     1000
   );
-  camera.position.set(0, 40, 0);
+  camera.position.set(0, 30, 0);
   camera.lookAt(0, 0, 0);
   scene.add(camera);
 
@@ -36,22 +38,75 @@ function main() {
   const ambientLight = new THREE.AmbientLight(0x080808);
   scene.add(ambientLight);
 
-  const controls = new THREE.OrbitControls(camera, renderer.domElement);
+  //const controls = new THREE.OrbitControls(camera, renderer.domElement);
 
   regenerateMaze();
-  const floorGeometry = new THREE.BoxBufferGeometry(1,1,1);
-  const floorMaterial = new THREE.MeshPhongMaterial({ color: 0x111111, emissive: 0x000000, specular: 0x333333, shininess: 30 });
+  const floorGeometry = new THREE.BoxBufferGeometry(1, 1, 1);
+  const floorMaterial = new THREE.MeshPhongMaterial({
+    color: 0x111111,
+    emissive: 0x000000,
+    specular: 0x333333,
+    shininess: 30
+  });
   const floorMesh = new THREE.Mesh(floorGeometry, floorMaterial);
-  floorMesh.scale.set((maze.length), 1, (maze[0].length));
-  floorMesh.position.set(0,0, 0);
+  floorMesh.scale.set(maze.length, 1, maze[0].length);
+  floorMesh.position.set(0, 0, 0);
   scene.add(floorMesh);
 
+  const playerGeometry = new THREE.SphereBufferGeometry(0.5, 32, 32);
+  const playerMaterial = new THREE.MeshPhongMaterial({
+    color: 0xff0000,
+    emissive: 0x000000,
+    specular: 0x333333,
+    shininess: 30
+  });
+  player = new THREE.Mesh(playerGeometry, playerMaterial);
+  player.position.set(0 - size / 2 + 0.5, 1, 1 - size / 2 + 0.5);
+  scene.add(player);
+
+  document.addEventListener("keydown", onDocumentKeyDown, false);
   loop(scene, camera, renderer);
 }
 
+function onDocumentKeyDown(event) {
+  const keyCode = event.which;
+  console.log(keyCode);
+  if (keyCode === 87 || keyCode === 38) {
+    if(maze[playerPosition.x][playerPosition.z-1]) {
+      playerPosition.z -= 1;
+      player.position.z -= 1;
+    }
+  } else if (keyCode === 83 || keyCode === 40) {
+    if(maze[playerPosition.x][playerPosition.z+1]) {
+      playerPosition.z += 1;
+      player.position.z += 1;
+    }
+  } else if (keyCode === 65 || keyCode === 37) {
+    if(maze[playerPosition.x-1][playerPosition.z]) {
+      playerPosition.x -= 1;
+      player.position.x -= 1;
+    }
+  } else if (keyCode === 68 || keyCode === 39) {
+    if(maze[playerPosition.x+1][playerPosition.z]) {
+      playerPosition.x += 1;
+      player.position.x += 1;
+    }
+  }
+  if(playerPosition.x === maze.length-1 && playerPosition.z === maze[0].length-2) {
+    alert("You win!");
+  }
+}
+
+function loop(scene, camera, renderer) {
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(function() {
+    loop(scene, camera, renderer);
+  });
+}
+
 function regenerateMaze() {
-  if(walls) {
-    for(let i = 0; i < walls.length; i++) {
+  if (walls) {
+    for (let i = 0; i < walls.length; i++) {
       scene.remove(walls[i]);
       walls[i].geometry.dispose();
       walls[i].material.dispose();
@@ -67,6 +122,12 @@ function regenerateMaze() {
       }
     }
   }
+}
+
+function reset() {
+  regenerateMaze();
+  playerPosition = {x: 0, z:1};
+  player.position.set(0 - size / 2 + 0.5, 1, 1 - size / 2 + 0.5);
 }
 
 function generateMaze() {
@@ -111,15 +172,19 @@ function generateMaze() {
       options.push({ row: current.row, col: current.col + 2 });
     }
     var index = -1;
-    if(options.length !== 0) {
+    if (options.length !== 0) {
       stack.push(current);
       index = Math.floor(Math.random() * options.length);
       const chosen = options[index];
-      maze[chosen.row + ((current.row - chosen.row)/2)][chosen.col + ((current.col - chosen.col)/2)] = 1;
+      maze[chosen.row + (current.row - chosen.row) / 2][
+        chosen.col + (current.col - chosen.col) / 2
+      ] = 1;
       visited.add(chosen.row * maze.length + chosen.col);
-      stack.push({ row: chosen.row, col: chosen.col});
+      stack.push({ row: chosen.row, col: chosen.col });
     }
   }
+  maze[0][1] = 1;
+  maze[maze.length - 1][maze[0].length - 2] = 1;
   return maze;
 }
 
@@ -129,16 +194,14 @@ function getIndex(position, maze) {
 
 function createCube(row, col, scene) {
   const geometry = new THREE.BoxBufferGeometry(1, 1, 1);
-  const material = new THREE.MeshPhongMaterial({ color: 0x777777, emissive: 0x000000, specular: 0x333333, shininess: 30 });
+  const material = new THREE.MeshPhongMaterial({
+    color: 0x777777,
+    emissive: 0x000000,
+    specular: 0x333333,
+    shininess: 30
+  });
   const mesh = new THREE.Mesh(geometry, material);
-  mesh.position.set(row-(size/2)+.5, 1, col-(size/2)+.5);
+  mesh.position.set(row - size / 2 + 0.5, 1, col - size / 2 + 0.5);
   scene.add(mesh);
   return mesh;
-}
-
-function loop(scene, camera, renderer) {
-  renderer.render(scene, camera);
-  window.requestAnimationFrame(function() {
-    loop(scene, camera, renderer);
-  });
 }
